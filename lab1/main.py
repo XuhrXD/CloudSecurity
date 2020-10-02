4d60fc376f45526e1bd43193c453cb4dbb61944f
XuhrXD <1420792746@qq.com>
2020-10-02 00:16:38 -0700

    lab2

--- lab1/main.py
+++ lab1/main.py
@@ -13,10 +13,12 @@
 # limitations under the License.
 
 # [START gae_python38_app]
-from flask import Flask, render_template, jsonify, Response, request
-from flask import send_from_directory
+from flask import Flask, render_template, jsonify, Response, request, flash
+from flask import send_from_directory, url_for, redirect, make_response
 from google.cloud import datastore
-import json, os
+from functools import wraps
+from base64 import b64encode
+import json, os, bcrypt, datetime
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -28,24 +30,125 @@ if os.getenv('GAE_ENV','').startswith('standard'):
 else:
     ROOT = DS.key('Entities', 'dev')
 
+def login_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        username = request.cookies.get('user')
+        session_id = request.cookies.get('session')
+
+        if username:
+            q_key = DS.key('Users', username)
+            q = DS.query(kind='Session', ancestor=q_key).fetch()
+
+            for val in list(q):
+                #print("database:"+val['Session_ID'])
+                #print(session_id)
+                if val['Session_ID']==session_id:
+                    return func(*args, **kwargs)
+        else:
+            print("Please log in")
+            return redirect(url_for('login'))
+
+        print('Session Error: Unmatched user session')
+        return redirect(url_for('login'))
+
+    return wrapper
+
+@app.route('/')
+@login_required
+def root():
+    return send_from_directory('static','index.html')
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method=='GET':
+        return send_from_directory('static','login.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = (request.form.get('password')).encode()
+        key = DS.key('Users', username)
+        entity = DS.query(kind='Users', ancestor=key).fetch()
+        for ent in list(entity):
+            if ent['Username'] == username and ent['Password'] == pwd_stretch(password, ent['Password']):
+                return createSession(username)
+            else:
+                print("Username or password is incorrect. Please try again")
+                return redirect(url_for('login'))
+        else:
+            print("Username or password does not match our records. Please try again")
+            return redirect(url_for('login'))
+
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return send_from_directory('static','signup.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = (request.form.get('password')).encode()
+        q_key = DS.key('Users', username)
+        user = DS.query(kind='Users', ancestor=q_key)
+
+        for ent in list(user.fetch()):
+            if ent['Username']==username:
+                print('Username already exists. Please choose a different username.')
+                return redirect(url_for('register'))
+        
+        hashed = pwd_stretch(password)
+        with DS.transaction():
+            user = datastore.Entity(key=q_key)
+            user.update({
+                'Username': username,
+                'Password': hashed
+            })
+            DS.put(user)
+
+        if username == 'admin':
+            migrate_data(username)
+        
+        return createSession(username)
+
+@app.route('/logout')
+def logout():
+    user = request.cookies.get('user')
+    session = request.cookies.get('session')
+    q = DS.query(kind='Session', ancestor=DS.key('Users', user))
+    for x in list(q.fetch()):
+        if x['Session_ID'] == session:
+            DS.delete(x.key)
+            break
+    print('You have been signed out.')
+    expired = datetime.datetime.now() - datetime.timedelta(hours=1)
+    res = make_response(redirect(url_for('login')))
+    res.set_cookie('user', '', max_age=0, expires=expired, secure=True)
+    res.set_cookie('session', '', max_age=0, expires=expired, secure=True)
+    return res
+
+    
 @app.route('/events')
+@login_required
 def getEvents():
-    vals = DS.query(kind='event', ancestor=ROOT).fetch()
+    username = request.cookies.get('user')
+    p_key = DS.key('Users', username)
+    vals = DS.query(kind='event', ancestor=p_key).fetch()
     return jsonify({
         'events':[{'name': v['name'], 'date': v['date']} for v in vals],
         'error': None,
     })
     
 @app.route('/event', methods=['POST'])
+@login_required
 def addEvents():
+    username = request.cookies.get('user')
+    p_key = DS.key('Users', username)
     data = request.json
-    entity = datastore.Entity(key=DS.key('event', parent=ROOT))
+    entity = datastore.Entity(key=DS.key('event', parent=p_key))
     entity.update({
         'name': data['name'],
         'date': data['date'],
     })
     DS.put(entity)
-    vals = DS.query(kind='event', ancestor=ROOT).fetch()
+    vals = DS.query(kind='event', ancestor=p_key).fetch()
     return jsonify({
         'events': [{
             'name': v['name'],
@@ -54,19 +157,48 @@ def addEvents():
         'error': None,
     })
 
-#@app.route('/event/<int:event_id>', methods=['DELETE'])
-#def delEvents():
-#   DS.delete(DS.key('event', event_id, parent=ROOT))
-#    return ""
+def pwd_stretch(pwdStr, hash=None):
+    if hash==None:
+        return bcrypt.hashpw(pwdStr, bcrypt.gensalt(10))
+    else:
+        return bcrypt.hashpw(pwdStr, hash)
 
-@app.route('/')
-def root():
-    return send_from_directory('static','index.html')
+def createSession(username):
+    key = DS.key('Users', username)
+    session_id = b64encode(os.urandom(64)).decode()
+    delta = datetime.datetime.now() + datetime.timedelta(hours=1)
 
+    # Add session ID in Datastore under parent User key.
+    session = datastore.Entity(key=DS.key('Session', parent=key))
+    session.update({
+        'Session_ID': session_id
+    })
+    DS.put(session)
+
+    res = make_response(redirect(url_for('root')))
+    res.set_cookie('user', username, max_age=(60*60), expires=delta,secure=True)
+    res.set_cookie('session', session_id, max_age=(60*60), expires=delta, secure=True)
+    return res
+
+def migrate_data(username):
+    if username == 'admin':
+        old_key = DS.key('Entities', 'root')
+        new_key = DS.key('Users', username)
+        old_q = DS.query(kind='Event', ancestor=old_key)
+
+        for val in list(old_q.fetch()):
+            ent = datastore.Entity(key=DS.key('Event', parent=new_key))
+            ent.update({
+                'name': val['name'],
+                'date': val['date']
+            })
+            DS.put(ent)
+            DS.delete(val.key)  
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
+    app.config['SECRET_KEY'] = os.urandom(16)
     app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python38_app]
---
+++ lab1/writeup.md
@@ -0,0 +1,3 @@
+##  migration plan: 
+### When a user named "admin" is created, all previous events without a parent user will be added to admin's account, then delete these events in database.
+
