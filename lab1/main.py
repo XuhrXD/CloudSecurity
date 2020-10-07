cc5dc436b549f9ff48ba24a54875cc68c77078de
XuhrXD <1420792746@qq.com>
2020-10-07 00:21:56 -0700

    lab3 finished

--- lab1/main.py
+++ lab1/main.py
@@ -17,14 +17,19 @@ from flask import Flask, render_template, jsonify, Response, request, flash
 from flask import send_from_directory, url_for, redirect, make_response
 from google.cloud import datastore
 from functools import wraps
-from base64 import b64encode
-import json, os, bcrypt, datetime
+from base64 import b64encode, urlsafe_b64decode
+import json, os, bcrypt, datetime, hashlib, requests
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
 
 DS = datastore.Client()
+CLIENT_ID = '1039840456821-6fv3442v4jsq0sjlmt7lmtvn8ju21sa9.apps.googleusercontent.com'
+REDIRECT_URL = 'https://lab1-289801.ue.r.appspot.com/oidcauth'
+STATE = hashlib.sha256(os.urandom(1024)).hexdigest()
+NONCE = hashlib.sha256(os.urandom(1024)).hexdigest()
+
 if os.getenv('GAE_ENV','').startswith('standard'):
     ROOT = DS.key('Entities', 'root')
 else:
@@ -41,8 +46,7 @@ def login_required(func):
             q = DS.query(kind='Session', ancestor=q_key).fetch()
 
             for val in list(q):
-                #print("database:"+val['Session_ID'])
-                #print(session_id)
+                
                 if val['Session_ID']==session_id:
                     return func(*args, **kwargs)
         else:
@@ -62,7 +66,20 @@ def root():
 @app.route('/login',methods=['GET', 'POST'])
 def login():
     if request.method=='GET':
-        return send_from_directory('static','login.html')
+        delta = datetime.datetime.now() + datetime.timedelta(hours=1)
+        print(REDIRECT_URL)
+        res = make_response(
+            render_template(
+                'login.html',
+                client_id = CLIENT_ID,
+                state = STATE,
+                nonce = NONCE,
+                redirect_url = REDIRECT_URL
+            )
+        )
+        res.set_cookie('oidc_state', STATE, max_age=(60*60), expires=delta, domain='lab1-289801.ue.r.appspot.com', secure=True)
+        res.set_cookie('oidc_nonce', NONCE, max_age=(60*60), expires=delta, domain='lab1-289801.ue.r.appspot.com', secure=True)
+        return res
     elif request.method == 'POST':
         username = request.form.get('username')
         password = (request.form.get('password')).encode()
@@ -108,7 +125,48 @@ def register():
         
         return createSession(username)
 
+@app.route('/oidcauth', methods=['GET'])
+def oidcauth():
+    print(request.args['state'])
+    print(request.cookies.get('oidc_state'))
+    if request.args['state'] != request.cookies.get('oidc_state'):
+        print('Wrong CSRF state')
+        return redirect(url_for('login'))
+    else:
+        response = requests.post('https://oauth2.googleapis.com/token',{
+            'code': request.args['code'],
+            'client_id': CLIENT_ID,
+            'client_secret': DS.get(DS.key('secret', 'oidc'))['client-secret'],
+            'redirect_uri': REDIRECT_URL,
+            'grant_type': 'authorization_code'
+        })
+
+        json_token = response.json()
+        id_token = json_token['id_token']
+        _, body, _ = id_token.split('.')
+        body += '=' * (-len(body) % 4)
+        claims = json.loads(urlsafe_b64decode(body.encode('utf-8')))
+
+        username = claims['sub']
+        q_key = DS.key('Users', username)
+        user_q = DS.query(kind = 'Users', ancestor=q_key)
+
+        for ent in list(user_q.fetch()):
+            if ent['Username'] == username:
+                return createSession(username)
+
+        with DS.transaction():
+            user = datastore.Entity(key=q_key)
+            user.update({
+                'Username': username,
+                'email': claims['email'],
+                'Password': ''
+            })
+            DS.put(user)
+        return createSession(username)
+
 @app.route('/logout')
+@login_required
 def logout():
     user = request.cookies.get('user')
     session = request.cookies.get('session')
@@ -120,8 +178,10 @@ def logout():
     print('You have been signed out.')
     expired = datetime.datetime.now() - datetime.timedelta(hours=1)
     res = make_response(redirect(url_for('login')))
-    res.set_cookie('user', '', max_age=0, expires=expired, secure=True)
-    res.set_cookie('session', '', max_age=0, expires=expired, secure=True)
+    res.set_cookie('user', '', max_age=0, expires=expired, domain='lab1-289801.ue.r.appspot.com', secure=True)
+    res.set_cookie('session', '', max_age=0, expires=expired, domain='lab1-289801.ue.r.appspot.com', secure=True)
+    res.set_cookie('oidc_nonce', '', max_age=0, domain='lab1-289801.ue.r.appspot.com', expires=expired)
+    res.set_cookie('oidc_state', '', max_age=0, domain='lab1-289801.ue.r.appspot.com', expires=expired)
     return res
 
     
@@ -176,8 +236,8 @@ def createSession(username):
     DS.put(session)
 
     res = make_response(redirect(url_for('root')))
-    res.set_cookie('user', username, max_age=(60*60), expires=delta,secure=True)
-    res.set_cookie('session', session_id, max_age=(60*60), expires=delta, secure=True)
+    res.set_cookie('user', username, max_age=(60*60), expires=delta, domain='lab1-289801.ue.r.appspot.com', secure=True)
+    res.set_cookie('session', session_id, max_age=(60*60), expires=delta, domain='lab1-289801.ue.r.appspot.com', secure=True)
     return res
 
 def migrate_data(username):
